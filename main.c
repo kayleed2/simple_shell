7363e2ead568bd8a6d7d394c1ba1f2efb95bf68d
Adam Clear <3589@holbertonschool.com>
2021-11-29 20:54:30 +0000

    updated main.c with fun stuff

--- main.c
+++ main.c
@@ -3,14 +3,15 @@
  * main - Simple shell
  * Return: 0 on success
  */
-
 int main(void)
 {
 	size_t bufsize = 1024;
 	pid_t pid;
 	char *buffer = malloc(bufsize * sizeof(char));
-	int status, i;
+	int status, i, x;
 	char **commands;
+	char **thepath = pathfinder();
+	char *fullpath = malloc(sizeof(char) * 1024);
 	struct stat st;
 
 	if (buffer == NULL)
@@ -20,10 +21,16 @@ int main(void)
 	{
 		if (_strcmp(buffer, "exit\n") == 0)
 		{
-			while (commands[i])
+			free(buffer);
+			free(fullpath);
+			for (x = 0; thepath[x] != NULL; x++)
 			{
-				free(commands[i]);
-				i++;
+				free(thepath[x]);
+			}
+			free(thepath);
+			for (x = 0; commands[x] != NULL; x++)
+			{
+				free(commands[x]);
 			}
 			free(commands);
 			exit(0);
@@ -36,14 +43,32 @@ int main(void)
 		buffer[_strlen(buffer) - 1] = '\0';
 		commands = splitter(buffer);
 		if (stat(commands[0], &st) == 0)
+		{
 			pid = fork();
+			if (pid != 0)
+				wait(&status);
+			if (pid == 0)
+				execve(commands[0], commands, NULL);
+		}
 		else
-			printf("%s not found\n", commands[0]);
+		{
+			for (x = 0; thepath[x] != NULL; x++)
+			{
+				_strcpy(fullpath, thepath[x]);
+				_strcat(fullpath, "/");
+				_strcat(fullpath, commands[0]);
+				if (stat(fullpath, &st) == 0)
+				{
+					pid = fork();
+					break;
+				}
+			}
+		}
 		if (pid != 0)
 			wait(&status);
 		if (pid == 0)
 		{
-			execve(commands[0], commands, NULL);
+			execve(fullpath, commands, NULL);
 		}
 		printf("$ ");
 		free(buffer);
