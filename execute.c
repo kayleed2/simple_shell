ba54a7c39fa8570f298dffa7fc06b5eff9c37ec8
Kaylee Dao <3597@holbertonschool.com>
2021-12-01 14:07:51 -0800

    Revise to Take in Path Parameter

--- execute.c
+++ execute.c
@@ -1,10 +1,11 @@
 #include "header.h"
 /**
  * execute - executes the commands passed to it
+ * @path: Path to check
  * @commands: input string array of commands
  * Return: 0 on success, -1 on failure
  */
-int execute(char **commands)
+int execute(char *path, char **commands)
 {
 	pid_t pid;
 	int status;
@@ -13,6 +14,6 @@ int execute(char **commands)
 	if (pid != 0)
 		wait(&status);
 	if (pid == 0)
-		execve(commands[0], commands, NULL);
-return (0);
+		execve(path, commands, NULL);
+	return (0);
 }
