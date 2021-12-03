461786f2e3bcd23005efad3c9c1d1c3b540967a0
Kaylee Dao <3597@holbertonschool.com>
2021-12-02 17:10:17 -0800

    Back to OG

--- main.c
+++ main.c
@@ -4,30 +4,28 @@
  * Return: 0 on success
  */
 int main(void)
-{
-	size_t bufsize = 1024;
-	char *buffer = malloc(bufsize * sizeof(char));
-	int x, status, exitstatus = 0, i = 1;
-	char **commands, **thepath = pathfinder();
-	char *fullpath = malloc(sizeof(char) * 1024);
-	struct stat st;
+ {
+ 	size_t bufsize = 1024;
+ 	char *buffer = malloc(bufsize * sizeof(char));
+ 	int x, i = 0;
+ 	char **commands, **thepath = pathfinder();
+ 	char *fullpath = malloc(sizeof(char) * 1024);
+ 	struct stat st;
 
-	if ((buffer == NULL) || (fullpath == NULL))
-		free(fullpath), free(buffer), exit(-1);
+	if (buffer == NULL)
+		return (-1);
 	if (isatty(0))
 		_printf("($) ");
 	while (getline(&buffer, &bufsize, stdin) != -1)
 	{
+		i++;
 		buffer[_strlen(buffer) - 1] = '\0', commands = splitter(buffer);
 		if (_strcmp(buffer, "exit\n") == 0)
 			break;
 		if (_strcmp(buffer, "env\n") == 0)
 			printenv();
 		if (stat(commands[0], &st) == 0)
-		{
-			exitstatus = WEXITSTATUS(status);
 			execute(commands[0], commands);
-		}
 		else
 		{
 			for (x = 0; thepath[x] != NULL; x++)
@@ -37,23 +35,20 @@ int main(void)
 				if (stat(fullpath, &st) == 0)
 				{
 					execute(fullpath, commands);
-					exitstatus = WEXITSTATUS(status);
 					free(commands);
 					break;
-				}
-			}
-			if (thepath[x] == NULL)
+ 				}
+ 			}
+ 			if (thepath[x] == NULL)
 			{
-				exitstatus = 127;
-				_printf("./hsh: %d: %s: not found\n", i, commands[0]);
+ 				_printf("./hsh: %d: %s: not found\n", i, commands[0]);
 				if (isatty(0))
 					free(commands);
-				i++;
 			}
-		}
-		if (isatty(0))
-			_printf("($) ");
+ 		}
+ 		if (isatty(0))
+ 			_printf("($) ");
 	}
 	free(buffer), free(fullpath), free(thepath), free(commands);
-	return (exitstatus);
+	return (0);
 }
